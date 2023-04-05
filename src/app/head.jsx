31610181eb7aacf98d09c73f1ec071d312bb3107
sam11227 <salmanrasool458@gmail.com>
2023-04-05 23:18:12 +0500

    step up app install next-js

---
+++ src/app/head.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div>
+        <>
+            <title>Google Clone App</title>
+            <meta content='width=device-width , initial-scale=1'name='viewport'/>
+            <meta name="description" content= "Google clone created by nextjs 13" />
+            <link real="icon" href='/favicon.ico'/>
+        </>
+      
+    </div>
+  )
+}
+
+export default Head
